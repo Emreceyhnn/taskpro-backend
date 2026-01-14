44becbae446c1b014e7438361c45c7cc0f422d21
DESKTOP-CA43GQ2\Emre <emreceyhnn@gmail.com>
2026-01-14 16:57:40 +0300

    t

--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ import { setupServer } from "./server.js";
 import { createDirIfNotExists } from "./utils/createDirIfNotExists.js";
 
 const bootstrap = async () => {
+  await initMongoConnection();
   await createDirIfNotExists(TEMP_UPLOAD_DIR);
   await createDirIfNotExists(UPLOAD_DIR);
-  await initMongoConnection();
   setupServer();
 };
 
